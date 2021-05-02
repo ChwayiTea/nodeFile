970c95ea540fa0cb12e5fd280682ff296d071242
Chwayita Moyeni <70192155+ChwayiTea@users.noreply.github.com>
2021-05-02 21:36:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 
 //write to a file
-fs.writeFile('result/file.json', JSON.stringify('This is my first file content'), (err) =>{
-    if(err) throw err
-}) //creates a new file
+//fs.writeFile('result/file.json', JSON.stringify('This is my first file content'), (err) =>{
+    //if(err) throw err
+//}) //creates a new file
 
 //read from a file
-//fs.readFile('result/file.json', 'utf8', (err, content) => {
-// if(err) throw err
-// console.log(content)
-//})
+fs.readFile('result/file.json', 'utf8', (err, content) => {
+   if(err) throw err
+   console.log(content)
+})
 
 
 //append a file
@@ -18,14 +18,14 @@ fs.writeFile('result/file.json', JSON.stringify('This is my first file content')
 //})
 
 //JSON file with an array to append to an existing file
-fs.readFile('result/file.json', 'utf8', (err, content) => {
-    if(err) throw err
+//fs.readFile('result/file.json', 'utf8', (err, content) => {
+  //  if(err) throw err
 
-    let data = JSON.parse(content)
+ //   let data = JSON.parse(content)
 
-    data.push("This is my second content");
+  //  data.push("This is my second content");
 
-    fs.writeFile('result/file.json', JSON.stringify(data), (err) => {
-        if(err) throw err
- })
-})
+//    fs.writeFile('result/file.json', JSON.stringify(data), (err) => {
+     //   if(err) throw err
+// })
+//})
